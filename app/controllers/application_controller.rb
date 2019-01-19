fb73fbd19d38f92574f3f1d9304b6ee43808c4a5
Saint-Jean Germila <germila@noukod.com>
2019-01-19 16:12:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,54 @@
-
+#
+# require_relative '../../config/environment'
+#
+# class ApplicationController < Sinatra::Base
+#
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+#
+#   get '/' do
+#
+#   end
+#
+#   get '/article/:id'do
+#
+#   end
+#
+#   get '/articles/new' do
+#
+#   erb :new
+#   end
+#
+#   get '/articles' do
+#
+#   end
+#
+#   post '/articles' do
+#
+#   end
+#
+#   get '/articles/:id/edit' do
+#
+#   erb :edit
+#   end
+#
+#   patch '/articles/:id' do
+#
+#   end
+#
+#   post '/articles/:id/delete' do
+#
+#   end
+#
+# end
+# require "sinatra"
+# require "sinatra/activerecord"
+#
+# set :database, "sqlite3:myblogdb.sqlite3"
+#
+# require "./models"
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,39 +57,36 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+# index posts
+get '/' do
+	@posts = Post.all
+	erb :index
+end
 
-  get '/' do
+# create post
+post '/post' do
+	@post = Post.create(title: params[:title], body: params[:body])
+	redirect '/'
+end
 
-  end
+# show post
+get '/post/:id' do
+	@post = Post.find(params[:id])
+	erb :post_page
+end
 
-  get '/article/:id'do
+# update post
+put '/post/:id' do
+	@post = Post.find(params[:id])
+	@post.update(title: params[:title], body: params[:body])
+	@post.save
+	redirect '/post/'+params[:id]
+end
 
-  end
-
-  get '/articles/new' do
-
-  erb :new
-  end
-
-  get '/articles' do
-
-  end
-
-  post '/articles' do
-
-  end
-
-  get '/articles/:id/edit' do
-
-  erb :edit
-  end
-
-  patch '/articles/:id' do
-
-  end
-
-  post '/articles/:id/delete' do
-
-  end
-
+# delete post
+delete '/post/:id' do
+	@post = Post.find(params[:id])
+	@post.destroy
+	redirect '/'
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201201114) do
+ActiveRecord::Schema.define(version: 20190119153643) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "content"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
